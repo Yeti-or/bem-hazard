8ae88a6cbeb0a76574fac4d77be7019a6cb35d50
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2015-07-08 06:06:14 +0300

    Button2: Add disabled state.

--- common.blocks/button2/button2.js
+++ common.blocks/button2/button2.js
@@ -1,28 +1,62 @@
 var Button = Button2 = React.createClass({
+    propTypes: {
+        tabindex: React.PropTypes.number,
+        //TODO: declare all props
+        //TODO: type - enum
+        type: React.PropTypes.string
+    },
+    getDefaultProps: function() {
+        return {
+            tabindex: 0
+        }
+    },
+    getInitialState: function() {
+        return {
+            active: false,
+            hover: false,
+            focus: false
+        }
+    },
+    componentWillReceiveProps: function(nextProps) {
+        if (nextProps.disabled && this.state.focus) {
+            React.findDOMNode(this).blur()
+            this._onBlur()
+        }
+    },
     render: function() {
-        var //attrs
+        var
+            //mods
+            type = this.props.type,
+            size = this.props.size,
+            theme = this.props.theme,
+            disabled = this.props.disabled
+            //attrs
             attrs = {
-                tabIndex : this.props.tabindex || 0,
+                type: this.props.type || 'button',
                 id : this.props.id,
                 name : this.props.name,
                 title : this.props.title,
-                value : this.props.val
+                value : this.props.val,
+                //TODO: how to remove tabIndex instead of using -1
+                tabIndex : disabled ? -1 : this.props.tabindex,
+                //TODO: why disabled=true?
+                disabled: disabled,
+                'aria-disabled': disabled
             },
-            //mods
-            type = this.props.type || 'button',
-            size = this.props.size,
-            theme = this.props.theme,
             //content
-            content = this.props.children
+            content = this.text = this.props.children,
             //TODO: BEM naming plugin
-            block = 'button2'
+            block = 'button2',
             cls = block
                 + (theme ? ' ' + block + '_theme_' + theme : '')
                 + (size ? ' ' + block + '_size_' + size : '')
+                + (disabled ? ' ' + block + '_disabled_yes' : '')
                 + (this.state.hover ? ' ' + block + '_hovered_yes' : '')
                 + (this.state.focus ? ' ' + block + '_focused_yes' : '')
-                + (this.state.pressed ? ' ' + block + '_pressed_yes' : '')
+                + (this.state.active ? ' ' + block + '_pressed_yes' : '')
+            ,
             cls__text = block + '__' + 'text'
+
         return (
             <button className={cls} {...attrs}
                     onClick={this._onClick}
@@ -34,40 +68,35 @@ var Button = Button2 = React.createClass({
             </button>
         )
     },
-    getInitialState: function() {
-        return {
-            pressed: false,
-            hover: false,
-            focus: false
-        }
-    },
     _onClick: function(e) {
-        this.props.onClick && this.props.onClick(e)
+        this.props.disabled || this.props.onClick && this.props.onClick(e)
     },
     _onMouseenter: function() {
-        this.setState({hover: true})
+        this.props.disabled || this.setState({hover: true})
     },
     _onMouseleave: function() {
         this.setState({hover: false})
         //TODO: bindToDoc diff from native btn
-        this.setState({pressed: false})
+        this.setState({active: false})
     },
     _onMouseDown: function() {
-        this.setState({pressed: true})
+        this.setState({active: true})
     },
     _onMouseUp: function() {
-        this.setState({pressed: false})
+        this.setState({active: false})
     },
     _onKeyDown: function(e) {
         if (e.key === ' ' || e.key === 'Enter') {
-            this.setState({pressed: true})
+            this.setState({active: true})
         }
     },
     _onKeyUp: function(e) {
-        this.setState({pressed: false})
+        this.setState({active: false})
     },
     _onFocus: function() {
-        this.setState({focus: true})
+        if (!this.props.disabled) {
+            this.setState({focus: true})
+        }
     },
     _onBlur: function() {
         this.setState({focus: false})
