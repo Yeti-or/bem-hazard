a5059ab46d92a420ff70d2509593f787f7926bad
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2015-07-12 05:50:12 +0300

    Icon: Add icon block

---
+++ common.blocks/icon/icon.js
@@ -0,0 +1,8 @@
+    bh.match({block: 'icon'}, function(ctx, json) {
+        ctx
+            .tag('i')
+            .attrs({
+                'style': json.url && ('background-image:url(\'' + json.url + '\');'),
+                'aria-hidden': 'true'
+            });
+    });
---
+++ desktop.bundle/icon/icon.js
@@ -0,0 +1,12 @@
+//Could be generated
+
+var Icon = React.createClass({
+    __block: 'icon',
+    __matchers: bh.__matchers,
+    mixins: [BEM],
+    render: function() {
+        return this.__node()
+    },
+})
+
+bh.clearMatchers()
