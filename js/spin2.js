0d875ffd54c0696a5f6c9d8456a6ae7bee17dea0
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2015-07-29 00:37:24 +0300

    Spin2: make example interactive

--- js/spin2.js
+++ js/spin2.js
@@ -2,6 +2,24 @@ var BH = require('../desktop.blocks/bem/bem.js')
 var BEM = BH.BEM
 
 var Spin = require('../desktop.bundle/spin2/spin2.js')
+require('../desktop.bundle/check-button/check-button.js')
+
+BH.bh.match('example', function(ctx) {
+        ctx.match('spin2', function(ctxS, json) {
+            ctxS.mod('progress', ctx.mod('progress'), true)
+        })
+
+        ctx.muMods({
+            progress: true
+        })
+        ctx.match('check-button', function(check) {
+            check.bind({
+                onChange: function(e) {
+                    ctx.muMod('progress', e.target.checked)
+                }
+            })
+        })
+})
 
 var bemJson = {
     content: 'XXS, 16px|XS, 24px|S, 28px|M, 32px|L, 38px'.split('|').map(function(label) {
@@ -33,7 +51,7 @@ var bemJson = {
 var Example = BH.React.createClass({
     render: function() {
         return (
-            <BEM attrs={{style:{margin: 20}}}>
+            <BEM>
                 {bemJson}
             </BEM>
         )
